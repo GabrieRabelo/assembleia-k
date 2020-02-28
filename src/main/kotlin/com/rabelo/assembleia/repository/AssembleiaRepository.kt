85ab112321b8ad226447d2d8f4dda21b7fcd8ec3
gabrierabelo <gabriel.almeida97@gmail.com>
2020-02-28 15:37:18 -0300

    02/adiciona notacao de repository

--- src/main/kotlin/com/rabelo/assembleia/repository/AssembleiaRepository.kt
+++ src/main/kotlin/com/rabelo/assembleia/repository/AssembleiaRepository.kt
@@ -2,6 +2,8 @@ package com.rabelo.assembleia.repository
 
 import com.rabelo.assembleia.model.Assembleia
 import org.springframework.data.repository.reactive.ReactiveCrudRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface AssembleiaRepository : ReactiveCrudRepository<Assembleia, String>{
 }
