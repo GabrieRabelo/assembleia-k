9123c2fef9c2160e1993150803421c8979b589c9
gabrierabelo <gabriel.almeida97@gmail.com>
2020-02-28 15:19:06 -0300

    00/cria document para assembleia

---
+++ src/main/kotlin/com/rabelo/assembleia/model/Assembleia.kt
@@ -0,0 +1,11 @@
+package com.rabelo.assembleia.model
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
+
+@Document
+data class Assembleia(@Id var id: String?,
+                      val pautas: MutableCollection<Pauta>) {
+
+
+}
