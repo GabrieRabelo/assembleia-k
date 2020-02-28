15acec10cc09849f73fec30f76d067cf512062bf
gabrierabelo <gabriel.almeida97@gmail.com>
2020-02-28 08:56:37 -0300

    first commit

---
+++ src/main/kotlin/com/rabelo/assembleia/AssembleiaApplication.kt
@@ -0,0 +1,11 @@
+package com.rabelo.assembleia
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AssembleiaApplication
+
+fun main(args: Array<String>) {
+	runApplication<AssembleiaApplication>(*args)
+}
---
+++ src/test/kotlin/com/rabelo/assembleia/AssembleiaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.rabelo.assembleia
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AssembleiaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
