ada4e3795fc67ef7d77b69807003a04c6489939e
gabrierabelo <gabriel.almeida97@gmail.com>
2020-03-02 11:26:47 -0300

    03/cria teste para get de assembleia

---
+++ src/test/kotlin/com/rabelo/assembleia/controller/AssembleiaControllerTest.kt
@@ -0,0 +1,54 @@
+package com.rabelo.assembleia.controller
+
+import com.rabelo.assembleia.model.Assembleia
+import com.rabelo.assembleia.repository.AssembleiaRepository
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+import org.mockito.BDDMockito.given
+import org.mockito.Mockito
+import org.springframework.test.web.reactive.server.WebTestClient
+import reactor.core.publisher.Flux
+
+internal class AssembleiaControllerTest {
+
+    lateinit var webTestClient: WebTestClient
+    lateinit var assembleiaController: AssembleiaController
+    lateinit var assembleiaRepository: AssembleiaRepository
+
+
+    @BeforeEach
+    fun setUp() {
+        assembleiaRepository = Mockito.mock(AssembleiaRepository::class.java)
+        assembleiaController = AssembleiaController(assembleiaRepository)
+        webTestClient = WebTestClient.bindToController(assembleiaController).build()
+    }
+
+    @Test
+    fun post() {
+    }
+
+    @Test
+    fun get() {
+        given(assembleiaRepository.findAll())
+                .willReturn(Flux.just(Assembleia(null, null), Assembleia(null, null)))
+
+        webTestClient.get()
+                .uri("/assembleia")
+                .exchange()
+                .expectBodyList(Assembleia::class.java)
+                .hasSize(2)
+    }
+
+    @Test
+    fun getById() {
+    }
+
+    @Test
+    fun put() {
+    }
+
+    @Test
+    fun delete() {
+    }
+}
