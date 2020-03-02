0fc386dce7dcf2f0838bc7961c2d6ef8fce1b312
gabrierabelo <gabriel.almeida97@gmail.com>
2020-03-02 09:43:45 -0300

    03/cria get para assembleia

--- src/main/kotlin/com/rabelo/assembleia/controller/AssembleiaController.kt
+++ src/main/kotlin/com/rabelo/assembleia/controller/AssembleiaController.kt
@@ -5,6 +5,7 @@ import com.rabelo.assembleia.repository.AssembleiaRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import java.util.*
 
@@ -17,5 +18,10 @@ class AssembleiaController @Autowired constructor(private val repository: Assemb
     fun post(): Mono<Assembleia> {
         val assembleia = Assembleia(UUID.randomUUID().toString(), null)
         return repository.save(assembleia)
+    }
+
+    @GetMapping(produces = ["application/stream+json"])
+    fun get(): Flux<Assembleia> {
+        return repository.findAll()
     }
 }
