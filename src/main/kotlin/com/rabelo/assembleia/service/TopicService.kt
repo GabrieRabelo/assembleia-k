f1f59f4ed5232b68343faaa29ec67a88f05a078e
gabrierabelo <gabriel.almeida97@gmail.com>
2020-03-05 15:43:02 -0300

    05/refatora nome de classe service

--- src/main/kotlin/com/rabelo/assembleia/service/TopicService.kt
+++ src/main/kotlin/com/rabelo/assembleia/service/TopicService.kt
@@ -5,7 +5,7 @@ import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
 interface TopicService {
-    fun create(): Mono<Topic>
+    fun createTopic(): Mono<Topic>
     fun getTopicList(): Flux<Topic>
     fun getTopicById(id:String): Mono<Topic>
     fun update(id:String, pauta: Topic): Mono<Topic>
