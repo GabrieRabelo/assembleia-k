502cbd0d8995082d80d45d1e4b68e3039963e45b
gabrierabelo <gabriel.almeida97@gmail.com>
2020-03-09 13:21:53 -0300

    05/adiciona controller de pautas

--- src/main/kotlin/com/rabelo/assembleia/controller/TopicController.kt
+++ src/main/kotlin/com/rabelo/assembleia/controller/TopicController.kt
@@ -4,15 +4,18 @@ import com.rabelo.assembleia.model.Assembly
 import com.rabelo.assembleia.model.Topic
 import com.rabelo.assembleia.service.TopicServiceImpl
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import reactor.core.publisher.Flux
 
 @RestController
 class TopicController @Autowired constructor(private val topicService: TopicServiceImpl) {
 
-    @GetMapping(produces = ["application/stream+json"], value = ["/assembleia/{assemblyId}/pauta/"])
+    @PostMapping(value = ["/assembleia/{assemblyId}/pauta"])
+    fun createTopic(@PathVariable assemblyId: String, @RequestBody topic: Topic) {
+        topicService.createTopic(assemblyId, topic)
+    }
+
+    @GetMapping(produces = ["application/stream+json"], value = ["/assembleia/{assemblyId}/pauta"])
     fun getTopicList(@PathVariable assemblyId: String): Flux<Topic> {
         return topicService.getTopicList(assemblyId)
     }
