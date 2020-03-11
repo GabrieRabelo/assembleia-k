7d351c2d90f3452130bd557899ef5ff6df13a5e5
gabrierabelo <gabriel.almeida97@gmail.com>
2020-03-11 10:20:34 -0300

    05/refatora classe topic para ter id

--- src/main/kotlin/com/rabelo/assembleia/model/Topic.kt
+++ src/main/kotlin/com/rabelo/assembleia/model/Topic.kt
@@ -1,7 +1,5 @@
 package com.rabelo.assembleia.model
 
-import org.springframework.data.annotation.Id
-import org.springframework.data.mongodb.core.mapping.Document
-
-data class Topic(val description: String,
+data class Topic(var id: String?,
+                 val description: String,
                  var associates: MutableList<Associate>?)
